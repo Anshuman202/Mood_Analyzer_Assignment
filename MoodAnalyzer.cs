42f7154893c3f770b4b582af048dee0a85690031
Anshuman202 <asanshuman2@gmail.com>
2023-01-18 19:14:17 +0530

    mood analyze

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyzerAssignment
+{
+    public class MoodAnalyzer
+    {
+        public static string analyseMood(string message)
+        {
+
+            return message;
+        }
+    }
+}
