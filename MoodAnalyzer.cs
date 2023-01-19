ae8ea07663bf865934acf18ec90519eb7bfdc9ae
Anshuman202 <asanshuman2@gmail.com>
2023-01-19 22:46:37 +0530

    mood analyser for uc2

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -24,4 +24,36 @@ namespace ModeAnalyzerAssignment
         }
 
     }
+    public class MoodAnalyser
+    {
+
+        public string msg;
+
+        public MoodAnalyser(string msg1)
+        {
+            this.msg = msg1;
+
+        }
+
+        public string analyseMood1()
+        {
+            MoodAnalyser obja = new MoodAnalyser(msg);
+
+            if (obja.msg == null)
+            {
+
+
+
+                throw new MoodAnalyzerNullException("Invalid Mood");
+            }
+            else
+            {
+                Console.WriteLine("Message is not NULL" + obja.msg);
+            }
+
+            return obja.msg;
+        }
+
+    }
+
 }
